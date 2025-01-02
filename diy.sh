e7044b45dcd729c22971bc772516acd42acd746f
Deepsea-x <67573211+Deepsea-x@users.noreply.github.com>
2025-01-02 15:33:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,3 +2,5 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
