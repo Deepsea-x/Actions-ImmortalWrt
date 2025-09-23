7f38f6091206500f8ddf0cb2df73c2616f11efa5
Deepsea-x <67573211+Deepsea-x@users.noreply.github.com>
2025-09-23 12:54:44 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,4 +3,7 @@
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 # Modify default IP
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sudo sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' ./package/lean/default-settings/files/zzz-default-settings
